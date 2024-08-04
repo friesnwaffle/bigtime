78a4d411f1977289253a02fddbd2c318075180a9
Prince <friesnwaffle@gmail.com>
2024-08-05 00:26:07 +0530

    turn on gfonts and geoip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,5 @@
 <slot />
 
 <style>
-    /* @import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap'); */
+    @import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
 </style>
